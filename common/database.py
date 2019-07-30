72a75c4449c3739bba6435c0add1bfe646d876e9
unknown <ankitdongre96@gmail.com>
2019-07-30 18:54:09 +0100

    Changed MongoDB URI again

--- common/database.py
+++ common/database.py
@@ -1,7 +1,7 @@
 import pymongo
 
 class Database(object):
-	URI = "mongodb+srv://admin1:M0ngo123@mycluster-n3ig0.mongodb.net/test?retryWrites=true&w=majority"
+	URI = "mongodb+srv://@mycluster-n3ig0.mongodb.net/test?retryWrites=true&w=majority"
 	DATABASE = None
 
 	@staticmethod
