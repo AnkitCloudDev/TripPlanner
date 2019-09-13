e75f6ec57c7593021b62e03f8b3b48f8091fe8ac
unknown <ankitdongre96@gmail.com>
2019-09-13 13:22:46 +0100

    Removed Tests

--- test/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from models.users.user import User
- 
- 
-@pytest.fixture(scope='module')
-def new_user():
-    user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
-    return user
